a5dd675575955d81c53167df1c0f00fb4f78221a
Johnny Luo <lzqwebsoft@gmail.com>
2016-01-11 16:07:22 +0800

    更新解析逻辑

--- Python/tutorial/tutorial/spiders/qiushibaike.py
+++ Python/tutorial/tutorial/spiders/qiushibaike.py
@@ -3,7 +3,6 @@ import scrapy
 from datetime import datetime
 from tutorial.items import QiuShiItem
 
-
 class QiuShiBaiKe(scrapy.Spider):
     name = 'qiushibaike.com'
     allowed_domains = ['qiushibaike.com']
@@ -16,18 +15,18 @@ class QiuShiBaiKe(scrapy.Spider):
              item = QiuShiItem()
              author_sel =  article.xpath('div[contains(@class, "author")]/a')
              item['header'] = author_sel.xpath('img/@src').extract()          # 用户头像
-             item['author'] = "".join(author_sel.xpath('text()').extract()).strip()   # 用户名称
+             item['author'] = "".join(author_sel.xpath('h2/text()').extract()).strip()   # 用户名称
              content_sel = article.xpath('div[@class="content"]')
              item['content'] = content_sel.xpath('text()').extract()        # 内容
              item['created_at'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # content_sel.xpath('@title').extract()     # 内容创建日期
              item['thumb'] = article.xpath("div[contains(@class, 'thumb')]/a/img/@src").extract()  # 内容附件图片
-            
+
              yield item
 
         # 得到下一页链接地址
-        next_href = response.xpath('//div[contains(@class, "pagebar")]/div/a[contains(@class, "next")]/@href').extract()
-        url = "http://www.qiushibaike.com" + next_href[0].strip()
-        current_pageNo = response.xpath('//div[contains(@class, "pagebar")]/div/a[contains(@class, "current")]/text()').extract()
+        next_href = response.xpath("//ul[contains(@class, 'pagination')]/li/a/span[contains(@class, 'next')]/../@href").extract_first()
+        url = "http://www.qiushibaike.com" + next_href.strip()
+        current_pageNo = response.xpath('//ul[contains(@class, "pagination")]/li/span[contains(@class, "current")]/text()').extract()
         current = current_pageNo[0].strip()
         # 只爬取首页面的35页内容
         if(int(current)!=35):
